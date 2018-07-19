60bf46c385cee07ebbc8c1459d34f7ae19382ed3
Nujian Den Mark Meralpis <meralpisdenmark@gmail.com>
2018-07-19 20:34:13 +0800

    replace with new template

--- lib/generators/templates/globelabs_sms_rails_initializer.rb
+++
@@ -1,7 +0,0 @@
-GlobelabsSmsRails.configure do |config|
-	config.app_id = '' # add your globelabs app app_id
-	config.app_secret = '' # add your globelabs app app_secret
-	config.passphrase = '' # add the given passphrase(provided by globe telecom)
-	config.short_code = '' # add the app short code
-	config.api_version = 'v1'
-end
