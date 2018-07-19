4e0c0b6397f59ce9bc72c1a23b5068cbbcfbbded
Nujian Den Mark Meralpis <meralpisdenmark@gmail.com>
2018-07-19 20:36:03 +0800

    add rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,20 @@
+require 'bundler/setup'
 require 'globelabs_sms_rails'
+
+RSpec.configure do |config|
+  config.example_status_persistence_file_path = ".rspec_status"
+  config.expose_dsl_globally = true
+  config.disable_monkey_patching!
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.before(:all) do
+    GlobelabsSmsRails.configure do |config|
+      config.passphrase = ENV['GLABS_PASSPHRASE']
+      config.app_id = ENV['GLABS_APP_ID']
+      config.app_secret = ENV['GLABS_APP_SECRET']
+      config.short_code = ENV['GLABS_SHORT_CODE']
+    end
+  end
+end
