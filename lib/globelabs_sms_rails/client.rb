afc8f76964bf7fef6bf04e723007a9ba474df633
Nujian Den Mark Meralpis <denmarkmeralpis@gmail.com>
2018-11-12 14:48:53 +0800

    move to address & message args to send method)

--- lib/globelabs_sms_rails/client.rb
+++ lib/globelabs_sms_rails/client.rb
@@ -2,7 +2,7 @@ require 'net/http'
 require 'json'
 
 module GlobelabsSmsRails
-   class Client      
+   class Client
       attr_accessor :app_id, :app_secret, :passphrase, :short_code, :host, :address, :message
       MSG_PARAMS = {}
 
@@ -12,20 +12,20 @@ module GlobelabsSmsRails
          @passphrase = GlobelabsSmsRails.configuration.passphrase
          @short_code = GlobelabsSmsRails.configuration.short_code
          @host = "https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/#{short_code}/requests"
-         @address = options[:address]
-         @message = options[:message]
       end
 
-      def send
+      def send(options)
+         @address = options.fetch(:address)
+         @message = options.fetch(:message)
          uri = URI(host)
 
          request = Net::HTTP.post_form(uri, message_params)
          response = JSON.parse(request.body, symbolize_names: true)
 
          if response.has_key?(:http_code)
-            case response[:http_code].to_i         
+            case response[:http_code].to_i
             when 400
-               raise Errors::BadRequest, response[:error]               
+               raise Errors::BadRequest, response[:error]
             when 401
                raise Errors::Authentication, response[:error]
             else
@@ -33,16 +33,16 @@ module GlobelabsSmsRails
             end
          else
             response.merge!(http_code: '200')
-         end         
+         end
       end
 
       def self.send(options={})
          obj = self.new(options)
-         return obj.send
+         return obj.send(options)
       end
-      
+
       private
-   
+
       def message_params
          MSG_PARAMS['app_id'] = app_id
          MSG_PARAMS['app_secret'] = app_secret
@@ -50,6 +50,6 @@ module GlobelabsSmsRails
          MSG_PARAMS['address'] = address
          MSG_PARAMS['message'] = message
          MSG_PARAMS
-      end     
+      end
    end
 end
